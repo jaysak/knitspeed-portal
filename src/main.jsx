5d91a1b051e4015efbb06a7a952eebada6631648
jaysak <jaysak@hotmail.com>
2026-04-21 23:38:17 +0700

    Replace KnitspeedPortal with AuthSmokeTest component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import KnitspeedPortal from './StockPortal.jsx'
+import AuthSmokeTest from './AuthSmokeTest.jsx'
 import './index.css'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <KnitspeedPortal />
+    <AuthSmokeTest />
+    {/* <KnitspeedPortal /> */}
   </React.StrictMode>,
 )
